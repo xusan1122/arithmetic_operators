19e41294b9972aeb175d4b21b9acf15b97b682c0
unknown <xusanmuqumov@gmail.com>
2022-12-14 09:30:38 +0500

    09-test

--- bin/arithmetic009.dart
+++ bin/arithmetic009.dart
@@ -1,12 +1,12 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 
-int soon(int a, int b) {
-  int a = 3;
-  int b = 9;
+int func( int a, int b) {
+  int a = 1;
+  int b = 5;
   return (a*a+b*b);
 }
 
 void main() {
-  print(soon(3, 9));
+  print(func(1, 5));
 }
